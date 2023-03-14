70f13ac8fd97ced67dbfeb6d6c4e2cd10e9ac2de
Agilulf2001 <aezlm26@126.com>
2023-03-15 00:18:48 +0800

    Site updated: 2023-03-15 00:18:47

---
+++ typetest/lines.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+#define charNum 100
+int main() {
+    ifstream src("AChristmasCarol.txt", ios::in);
+    string str;
+    int i = 1000;
+    while (getline(src, str)) {
+        if (str.size() > charNum) {
+            string filename = "outs/out";
+            i++;
+            filename += to_string(i) + ".txt";
+            ofstream dst(filename, ios::out);
+            dst << str;
+            dst.close();
+        }
+    }
+    src.close();
+    return 0;
+}
